bb2a941d7adb2471809df3f2550124830e9ed5d9
Ollivier Robert <roberto@keltia.net>
2021-11-06 02:55:06 +0100

    Showcase for options.

---
+++ examples/showall.rs
@@ -0,0 +1,33 @@
+use log::info;
+use ipify::{Engine, Ipify, Op};
+
+fn doit(e: Engine) {
+    let a = Ipify::new().with(e);
+
+    let ip4 = a.set(Op::IP4).call();
+    let ip6 = a.set(Op::IP6).call();
+
+    println!("IP4={:?}", ip4);
+    println!("IP6={:?}", ip6);
+}
+
+fn main() {
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(2)
+        .init()
+        .unwrap();
+    info!("Start");
+
+    info!("Using defaults (ureq, ipv6)");
+    println!("IP={}", Ipify::new().call());
+
+    info!("Using defaults, get json");
+    println!("IP={}", Ipify::new().set(Op::IP6J).call());
+
+    info!("Using ureq");
+    doit(Engine::Reqw);
+
+    info!("Using reqwest");
+    doit(Engine::Reqw);
+}
