17c5dbb431cbb64a424476e95a29c6941e860ef7
Ollivier Robert <roberto@keltia.net>
2022-05-24 23:46:25 +0200

    Async version of showall.

---
+++ examples/async.rs
@@ -0,0 +1,30 @@
+use ipify_rs::*;
+use log::info;
+
+async fn doit() {
+    let ip = Ipify::new();
+
+    println!("IP4={:?}", ip.set(Op::IPv4).call_async().await);
+    println!("IP6={:?}", ip.set(Op::IPv6).call_async().await);
+}
+
+#[tokio::main]
+async fn main() {
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(2)
+        .init()
+        .unwrap();
+    info!("Start");
+
+    info!("Using default, minimal API");
+    println!("IP={}", myip());
+
+    info!("Using defaults (ipv6)");
+    println!("IP={}", Ipify::new().call_async().await);
+
+    info!("Using defaults, get json");
+    println!("IP={}", Ipify::new().set(Op::IPv6J).call_async().await);
+
+    doit().await;
+}
